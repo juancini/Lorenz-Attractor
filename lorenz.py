40e8cd88a24cc18c0cd5e8c66f49003979431268
Juan Mancini <juani.mancini98@gmail.com>
2023-03-24 00:31:53 -0300

    fixed missing quotation

--- lorenz.py
+++ lorenz.py
@@ -12,9 +12,9 @@ def lorenz(t, xyz, sigma=10, beta=8/3, rho=28):
 
 t_span = [0, 100]
 xyz0 = [1, 1, 1]
-
+print('STEP 1')
 sol = solve_ivp(lorenz, t_span, xyz0, t_eval=np.linspace(0, 100, 10000))
-
+print('step2')
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 ax.plot(sol.y[0], sol.y[1], sol.y[2])
@@ -22,3 +22,4 @@ ax.set_xlabel("X")
 ax.set_ylabel('Y')
 ax.set_zlabel('Z')
 plt.show()
+print('End')
